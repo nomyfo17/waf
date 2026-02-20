37d77c7aeaceca92bcc7b6927d0b4b59cc5a03e0
nomyfo17 <nomyfo171234@gmail.com>
2026-02-19 16:12:16 -0800

    Open🥞
    this is a opensource version of 🥞WAF anyone can edit this file once downloaded. this is not the offical  🥞WAF file but is the offical Open🥞

---
+++ open🥞.py
@@ -0,0 +1,37 @@
+import time
+import random
+
+#say = print
+#run = break/start(run)
+
+loc = []
+
+while True:
+    while True:
+        cmnd = input("Open 🥞 -  ")
+        if cmnd == "run":
+            break
+        elif cmnd == "clear":
+            loc.append(cmnd)
+            break
+        else:
+            loc.append(cmnd)
+    for cmnd in loc:
+        if loc.__contains__("txt"):
+            print("</>  " + cmnd)
+        if loc.__contains__("hw"):
+            print("</>  " + "Hello World!")
+        if loc.__contains__("clear"):
+            loc.clear()
+            os.system('clear')
+            print("</>  " + "history cleared")
+        if loc.__contains__("option"):
+            loc.clear()
+            print("</>  " + "txt = what you type will type back")
+            time.sleep(5)
+            print("</>  " + "hw = print Hello World")
+            time.sleep(5)
+            print("</>  " + "clear = clear past commands")
+            time.sleep(5)
+            print("</>  " + "search = command vocab")
+            time.sleep(5)
