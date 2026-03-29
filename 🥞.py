326e1349cb105128c3194d032c0fd83c5c2de457
nomyfo17 <nomyfo171234@gmail.com>
2026-03-29 16:55:03 -0700

    Update 🥞.py v1.6

--- 🥞.py
+++ 🥞.py
@@ -6,61 +6,40 @@ import os
 #ask = input
 #run = break/start(run)
 
+print("Welcome to WAF/🥞")
+
 loc = []
 
-while True:
-    while True:
-        cmnd = input("🥞 </> (  ")
-        if cmnd == "run":
-            print("</> (🥞) -1/2-")
-            break
-        elif cmnd.__contains__("name"):
-            loc.append(cmnd)
-            break
-        elif cmnd == "clear":
-            print(".  ")
-            time.sleep(0.5)
-            print(".. ")
-            time.sleep(0.5)
-            print("...")
-            time.sleep(0.5)
-            print(".  ")
-            time.sleep(0.5)
-            print(".. ")
-            time.sleep(0.5)
-            print("...")
-            time.sleep(0.5)
-            loc.append(cmnd)
-            break
-        else:
-            loc.append(cmnd)
-    print("</> (🥞) -  ")
-    for cmnd in loc:
-        if loc.__contains__("txt"):
-            print("</>  " + cmnd)
-        if loc.__contains__("hw"):
-            print("</>  " + "Hello World!")
-        if loc.__contains__("clear"):
-            loc.clear()
-            os.system('clear')
-            print("</>  " + "history cleared")
-        if loc.__contains__("option"):
-            loc.clear()
-            print("</>  " + "txt = what you type will type back")
-            time.sleep(3)
-            print("</>  " + "hw = print Hello World")
-            time.sleep(3)
-            print("</>  " + "clear = clear past commands")
-            time.sleep(3)
-            print("</>  " + "search = command vocab")
-            time.sleep(3)
-            print("</>  " + "name = file a new file")
-            time.sleep(3)
-            print("</>  " + "save = save a file must be under name")
-            time.sleep(3)
-        
-        if loc.__contains__("name"):
-            print("code or txt")
+def clear():
+    print(".  ")
+    time.sleep(0.1)
+    print(".. ")
+    time.sleep(0.1)
+    print("...")
+    time.sleep(0.1)
+    print(".  ")
+    time.sleep(0.1)
+    print(".. ")
+    time.sleep(0.1)
+    print("...")
+    time.sleep(0.1)
+
+def search():
+    print("</>  " + "txt = what you type will type back")
+    time.sleep(0.2)
+    print("</>  " + "hw = print Hello World")
+    time.sleep(0.2)
+    print("</>  " + "clear = clear past commands")
+    time.sleep(0.2)
+    print("</>  " + "search = command vocab")
+    time.sleep(0.2)
+    print("</>  " + "name = file a new file")
+    time.sleep(0.2)
+    print("</>  " + "save = save a file must be under name")
+    time.sleep(0.2)
+
+def name():
+    print("code or txt")
     time.sleep(0.2)
     fullcode = []
     while True:
@@ -76,3 +55,62 @@ while True:
         for item in fullcode:
             file.write(str(item) + "\n")
     print("file sussecfullly saved!")
+
+def txt():
+    for cmnd in loc:
+        print("</>  " + cmnd)
+
+def hw():
+    print("</>  " + "Hello World!")
+
+def clear():
+    loc.clear()
+    os.system('clear')
+    print("</>  " + "history cleared")
+
+def run():
+    print("</> (🥞) -1/2-")
+
+def name():
+    print("code or txt")
+    time.sleep(0.2)
+    fullcode = []
+    def names2():
+        while True:
+            time.sleep(0.5)
+            code = input(" -  ")
+            if code == "save":
+                break
+            else:
+                fullcode.append(code)
+        time.sleep(0.2)
+        filename = input("file name -  ")
+        with open(filename, "a") as file:
+            for item in fullcode:
+                file.write(str(item) + "\n")
+        print("file sussecfullly saved!")
+    names2()
+
+
+while True:
+    while True:
+        cmnd = input("🥞 </> (  ")
+        if cmnd == "run":
+            run()
+            break
+        else:
+            loc.append(cmnd)
+    print("</> (🥞) -  ")
+    for cmnd in loc:
+        if loc.__contains__("txt"):
+            txt()
+        if loc.__contains__("hw"):
+            hw()
+        if loc.__contains__("clear"):
+            loc.clear()
+            clear()
+        if loc.__contains__("search"):
+            search()
+        
+        if loc.__contains__("name"):
+            name()
